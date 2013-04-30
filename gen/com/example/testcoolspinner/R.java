ee124681d5c543f6eac97a2c876e6d074610b380
Don Nguyen <don.nguyen@hazuu.com>
2013-04-30 11:21:28 +0700

    Change inner loading

--- gen/com/example/testcoolspinner/R.java
+++ gen/com/example/testcoolspinner/R.java
@@ -25,10 +25,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int spinner=0x7f020000;
-        public static final int spinner_inner=0x7f020001;
-        public static final int spinner_inner_1=0x7f020002;
-        public static final int spinner_outer=0x7f020003;
+        public static final int ic_loading=0x7f020000;
+        public static final int spinner=0x7f020001;
+        public static final int spinner_inner=0x7f020002;
+        public static final int spinner_inner_1=0x7f020003;
+        public static final int spinner_outer=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f090002;
